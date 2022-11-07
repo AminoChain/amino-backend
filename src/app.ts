e6afb841b698274ca9c2516064a6e236b701f4a8
yurii <sllouyssgort@gmail.com>
2022-11-07 19:08:42 +0100

    Updates

--- src/app.ts
+++ src/app.ts
@@ -55,71 +55,54 @@ const encryptor = new Encryptor(hlaEncodingKey)
 
 
 app.post('/register-donation', async (req: Request, res: Response) => {
-    res.status(200).end()
-    return
-
     const data = req.body as BiobankRegistrationData
     const { hla, amounts, biobankAddress, donorAddress, genome, signature} = data
 
     const authenticator = await getAuthenticatorContract()
 
     const hlaHash = ethers.utils.id(JSON.stringify(hla))
+    const hlaEncoded = encryptor.encrypt(JSON.stringify(hla))
 
-    // reduce long operation
-    const registrationParametersHash = '0xa83a0d199caab0a87cbc094b8a88acb6b570176ad5cb374cf576768712b52151'
-    /*const registrationParametersHash = await authenticator.getRegistrationHash(
+    const registrationParametersHash = await authenticator.getRegistrationHash(
         donorAddress,
         hlaHash
-    )*/
+    )
 
     const digest = arrayify(registrationParametersHash)
     const recoveredAddress = recoverAddress(digest, signature)
 
     if (recoveredAddress.localeCompare(donorAddress) !== 1) {
-        res.status(403)
+        res.status(403).end()
         return
     }
 
-    // reduce long operation
-    // const genomeEncodedIpfsId = await uploadGenomeToIpfs(genome)
-    const genomeEncodedIpfsId = 'bafybeihfkmtsraiwkdkb7pc7ltmmsiqawoozzbjtcanilbykpv6trj5m7y'
+    const genomeEncodedIpfsId = await uploadGenomeToIpfs(genome)
 
-    // reduce long operation
-    const hlaEncoded = mockHlaEncoded
-    // const hlaEncoded = encryptor.encrypt(JSON.stringify(hla))
-
-    // reduce long operation
     const hlaHashed = {
-        "A": "0xb4ff4867cc79126826f7a227459583daf153a4c06ebe46aa70d02b5edf79bf90",
-        "B": "0x4f78dcb8885880abdd79a0ee42acd93e663070f9f26bc0fc0c5f286292ed80b8",
-        "C": "0x91a4b0333b4164454a16c893710e4d93be09adce949dd57b62045d9702889926",
-        "DPB": "0x2bf8860cf9668ead6525698f0457f7848f42305ccbc59f6880f5eee34ac8283f",
-        "DRB": "0x8a2488f5299e815401467583e7495c69909605d3670c05471bc064e9d0938228"
-    }
-    /*const hlaHashed = {
         A: ethers.utils.id(hla.A.toString()), // use ethers.utils.id instead
         B: ethers.utils.id(hla.B.toString()),
         C: ethers.utils.id(hla.C.toString()),
         DPB: ethers.utils.id(hla.DPB.toString()),
         DRB: ethers.utils.id(hla.DRB.toString()),
-    }*/
+    }
 
     try {
         const tx = await authenticator.register({
             hlaHashed,
             hlaHash,
             hlaEncoded,
-            genomeEncodedIpfsId, //: 'bafybeihfkmtsraiwkdkb7pc7ltmmsiqawoozzbjtcanilbykpv6trj5m7y', // encode and upload `genome` to IPFS
+            genomeEncodedIpfsId,
             amounts,
             donor: donorAddress,
             biobank: biobankAddress
-        }, { gasLimit: 100_000 })
+        })
+        // reduce long operation because timeout could be reach
         // const receipt = await tx.wait()
         console.log('Registration tx: '+tx.hash)
-        res.status(200).send(tx.hash)
+        res.status(200).end(tx.hash)
     } catch (e) {
         console.error(e)
-        res.status(500)
+        res.status(500).end()
     }
 
 
@@ -234,86 +217,3 @@ export async function uploadGenomeToIpfs(genome: string) {
     console.log(cid)
     return cid as string
 }
-
-const mockHlaEncoded = new Uint8Array([
-    69,
-    8,
-    5,
-    3,
-    8,
-    0,
-    55,
-    7,
-    9,
-    1,
-    134,
-    229,
-    34,
-    159,
-    31,
-    11,
-    82,
-    79,
-    189,
-    151,
-    42,
-    206,
-    245,
-    70,
-    247,
-    126,
-    91,
-    168,
-    253,
-    84,
-    136,
-    93,
-    42,
-    173,
-    251,
-    39,
-    91,
-    232,
-    154,
-    146,
-    2,
-    173,
-    35,
-    68,
-    254,
-    234,
-    99,
-    202,
-    57,
-    178,
-    109,
-    65,
-    250,
-    46,
-    47,
-    141,
-    163,
-    209,
-    148,
-    144,
-    84,
-    204,
-    138,
-    78,
-    127,
-    193,
-    103,
-    111,
-    65,
-    184,
-    181,
-    73,
-    141,
-    117,
-    209,
-    188,
-    153,
-    143,
-    145,
-    156
-])
