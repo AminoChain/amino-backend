909964ca4a234b1f883d35dc769cbda0f2e6c5c0
yurii <sllouyssgort@gmail.com>
2022-11-07 22:03:14 +0100

    Updates

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,8 @@ import {BigNumberish, BytesLike, CallOverrides, Contract, ContractTransaction, e
 import express, {Application, raw, Request, Response} from 'express'
 // @ts-ignore
 import AminoChainAuthenticatorArtifact from './artifacts/AminoChainAuthenticator.sol/AminoChainAuthenticator.json'
-import { AminoChainAuthenticator } from '../../amino-contracts/typechain/contracts'
+import AminoChainDonationArtifact from './artifacts/AminoChainDonation.sol/AminoChainDonation.json'
+import { AminoChainAuthenticator, AminoChainDonation } from '../../amino-contracts/typechain/contracts'
 import {Encryptor} from "./encryptor";
 
 import * as dotenv from 'dotenv'
@@ -163,35 +164,62 @@ app.post('/register-donation', async (req: Request, res: Response) => {
     }
 })*/
 
-app.get('/get-bio-data/:biodataHash', async (req: Request, res: Response) => {
-    const { biodataHash} = req.params
+app.get('/decode-hla/:tokenId', async (req: Request, res: Response) => {
+    const { tokenId} = req.params
 
-    /*const authenticator = await getAuthenticatorContract()
+    const nft = await getNftContract()
 
-    const storedBioDataEncoded = await authenticator.bioDataEncoded(biodataHash)
-    if (storedBioDataEncoded === '0x') {
-        res.status(200).send('')
+    const hlaEncoded = await nft.getHlaEncoded(tokenId)
+    if (hlaEncoded === '0x') {
+        res.status(200).send('').end()
     } else {
-        const storedBioData = encryptor.decrypt(ethers.utils.arrayify(storedBioDataEncoded))
+        const hla = encryptor.decrypt(ethers.utils.arrayify(hlaEncoded))
 
-        res.status(200).send(storedBioData)
-    }*/
+        res.status(200).send(hla).end()
+    }
 })
 
-const server = app.listen(port, function () {
+app.get('/decode-genome/:tokenId', async (req: Request, res: Response) => {
+    const { tokenId} = req.params
+
+    const nft = await getNftContract()
+
+    const genomeEncodedIpfsId = await nft.getGenomeEncodedUrl(tokenId)
+    if (genomeEncodedIpfsId === '') {
+        res.status(200).end()
+    } else {
+        const url = `https://${genomeEncodedIpfsId}.ipfs.w3s.link/ipfs/${genomeEncodedIpfsId}/file`
+        const response = await fetch(url)
+        const encoded = await response.arrayBuffer()
+        const decoded = encryptor.decrypt(new Uint8Array(encoded))
+
+        res.status(200).send(decoded).end()
+    }
+})
+
+app.listen(port, function () {
     console.log(`App is listening on port http://localhost:${port} !`)
 })
 
-async function getAuthenticatorContract() {
-    const provider = new ethers.providers.JsonRpcProvider('https://rpc-mumbai.maticvigil.com')
-    const signer = new ethers.Wallet(platformWalletPk, provider)
+const provider = new ethers.providers.JsonRpcProvider('https://rpc-mumbai.maticvigil.com')
+const signer = new ethers.Wallet(platformWalletPk, provider)
 
+async function getAuthenticatorContract() {
     const contract = new Contract(
         '0xe678C9BA5a9aE61fAc009a602b29ed869eD8156c',
         AminoChainAuthenticatorArtifact.abi,
         signer
     )
     return await contract.deployed() as unknown as AminoChainAuthenticator
+}
+
+async function getNftContract() {
+    const contract = new Contract(
+        '0xdF20EDD683d4a5e14a7B37eC9fDc6884d07Ba92E',
+        AminoChainDonationArtifact.abi,
+        signer
+    )
+    return await contract.deployed() as unknown as AminoChainDonation
 }
 
 
